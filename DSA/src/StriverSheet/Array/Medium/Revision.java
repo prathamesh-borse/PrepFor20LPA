57db61446373f76474d88c0c2689b9dd49465376
Prathamesh Borse <prathameshborse.official@gmail.com>
2026-02-16 20:01:02 +0530

    Added Leetcode Problems

--- DSA/src/StriverSheet/Array/Medium/Revision.java
+++ DSA/src/StriverSheet/Array/Medium/Revision.java
@@ -1,42 +1,86 @@
 package StriverSheet.Array.Medium;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
 public class Revision {
     public static void main(String[] args) {
-        int[] arr = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
-        int ans = printSubarrayofMaxiumSubarraySum(arr);
-        System.out.println(ans);
+        int[][] arr = {{0, 1, 2, 0}, {3, 4, 5, 2}, {1, 3, 1, 5}};
+        setZeroesBetter(arr);
+        printArray(arr);
     }
 
-    public static int printSubarrayofMaxiumSubarraySum(int[] arr) {
-        int n = arr.length, maxiSum = Integer.MIN_VALUE, currentSum = 0, start = 0;
-        int ansStart = -1, ansEnd = -1;
-        for (int left = 0; left < n; left++) {
-            if (left == 0) {
-                start = left;
-            }
-
-            currentSum += arr[left];
-
-            if (currentSum > maxiSum) {
-                maxiSum = currentSum;
-                ansStart = start;
-                ansEnd = left;
-            }
-
-            if (currentSum < 0) {
-                currentSum = 0;
-                start = left + 1;
+    public static void setZeroesBetter(int[][] matrix) {
+        int n = matrix.length; // number of rows
+        int m = matrix[0].length; // number of columns
+        int[] row = new int[n];
+        int[] column = new int[m];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (matrix[i][j] == 0) {
+                    row[i] = 1;
+                    column[j] = 1;
+                }
             }
         }
-        List<Integer> ans = new ArrayList<>();
-        for (int i = ansStart; i <= ansEnd; i++) {
-            ans.add(arr[i]);
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (row[i] == 1 || column[j] == 1) {
+                    matrix[i][j] = 0;
+                }
+            }
         }
-        System.out.println(Arrays.asList(ans));
-        return maxiSum;
     }
+
+    private static void printArray(int[][] arr) {
+        int n = arr.length, m = arr[0].length;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void setZeroes(int[][] matrix) {
+        int n = matrix.length, m = matrix[0].length;
+        // column = n, row = m;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (matrix[i][j] == 0) {
+                    markRow(matrix, i);
+                    markColumn(matrix, j);
+                }
+            }
+        }
+        markAllMinusOne(matrix);
+    }
+
+    private static void markRow(int[][] matrix, int i) {
+        int m = matrix[0].length;
+        for (int j = 0; j < m; j++) {
+            if (matrix[i][j] != 0) {
+                matrix[i][j] = -1;
+            }
+        }
+    }
+
+    private static void markColumn(int[][] matrix, int j) {
+        int n = matrix.length;
+        for (int i = 0; i < n; i++) {
+            if (matrix[i][j] != 0) {
+                matrix[i][j] = -1;
+            }
+        }
+    }
+
+    private static void markAllMinusOne(int[][] matrix) {
+        int n = matrix.length, m = matrix[0].length;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (matrix[i][j] == -1) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+    }
+
 }
--- DSA/src/StriverSheet/Array/Medium/RotateMatrixBy90Degrees.java
+++ DSA/src/StriverSheet/Array/Medium/RotateMatrixBy90Degrees.java
@@ -3,11 +3,22 @@ package StriverSheet.Array.Medium;
 public class RotateMatrixBy90Degrees {
     public static void main(String[] args) {
         int[][] arr = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
-        int[][] ans = rotateOptimal(arr);
+        int[][] ans = rotate(arr);
         printMatrix(ans);
     }
 
-    private static int[][] rotateOptimal(int[][] arr) {
+    private static void printMatrix(int[][] matrix) {
+        int n = matrix.length;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    private static int[][] rotate(int[][] arr) {
+        // transpose the matrix
         int n = arr.length;
         for (int i = 0; i < n; i++) {
             for (int j = i + 1; j < n; j++) {
@@ -15,6 +26,7 @@ public class RotateMatrixBy90Degrees {
             }
         }
 
+        // reverse the matrix
         for (int i = 0; i < n; i++) {
             int left = 0, right = n - 1;
             while (left < right) {
@@ -25,32 +37,14 @@ public class RotateMatrixBy90Degrees {
                 right--;
             }
         }
+
         return arr;
     }
 
-    private static void swap(int[][] arr, int i, int j) {
-        int temp = arr[i][j];
-        arr[i][j] = arr[j][i];
-        arr[j][i] = temp;
+    private static void swap(int[][] mat, int i, int j) {
+        int temp = mat[i][j];
+        mat[i][j] = mat[j][i];
+        mat[j][i] = temp;
     }
 
-    private static void printMatrix(int[][] ans) {
-        for (int i = 0; i < ans.length; i++) {
-            for (int j = 0; j < ans.length; j++) {
-                System.out.print(ans[i][j] + " ");
-            }
-            System.out.println();
-        }
-    }
-
-    private static int[][] rotateBrute(int[][] arr) {
-        int n = arr.length;
-        int[][] ans = new int[n][n];
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                ans[j][n - 1 - i] = arr[i][j];
-            }
-        }
-        return ans;
-    }
 }
